1a2c7079265013f472686f2acf9e687eb7da2452
Lennaart Raams <l.raams@gerimedica.nl>
2024-06-26 22:42:58 +0200

    fixed term replacement

--- medical_terms.py
+++ medical_terms.py
@@ -9,51 +9,28 @@ import re
 from tqdm import tqdm
 tqdm.pandas(desc="processing vumc dataextract")
 
-# df.to_csv('terms.csv',index=None)
 abbreviations_df = pd.read_csv('./data/terms.csv',index_col=None)
 abbreviations_df = abbreviations_df.loc[abbreviations_df['status']=='enable']
-abbreviations_df.info()
 
 abbr_list = abbreviations_df.to_dict(orient='records')
 
 
-def replace_abbreviations(text:str, abbreviations_df):
-    for abbr in abbr_list:
+def replace_abbreviations_dataframe(df:pd.DataFrame, abbr_list) -> pd.DataFrame:
+    for abbr in tqdm(abbr_list):
         abbreviation = abbr['term']
         replacement = abbr['meaning']
         ignore_case = abbr.get('ignore_case', False)
 
         flags = re.IGNORECASE if ignore_case else 0
 
-        pattern = r'(?=<^|\s|){}(?=[.><+]|\s|$)'.format(re.escape(abbreviation))
-
-        # Replace abbreviations in the column using lambda function
-        # df['text'] = df[label].progress_apply(lambda text: re.sub(pattern, replacement, text, flags=flags))
+        pattern2 = r'(<^|\s|){}([.><+]|\s|$)'.format(re.escape(abbreviation))
+        df['text'] = df['diagnose'].progress_apply(lambda t: re.sub(pattern2,rf'\1{replacement}\2',t,flags=flags))
         
-        text = re.sub(pattern,replacement, text, flags=flags)
+    return df
 
-    return text
-
-
-
-df_text = pd.DataFrame(['this is a text', 'this is another text'], columns=['text'])
 df_text = pd.read_csv('./data/medische_voorgeschiedenis.csv', index_col='db_id', sep='|')
 df_text = df_text.loc[~df_text['diagnose'].isna()]
 
-df_text['text'] = df_text['diagnose'].progress_apply(lambda text: replace_abbreviations(text,abbreviations_df))
+replace_abbreviations_dataframe(df_text, abbr_list)
+df_text.to_csv('./data/medische_voorgeschiedenis_processed.csv',index=None)
 
-pattern = r'(?=<^|\s|){}(?=[.><+]|\s|$)'.format(re.escape('re'))
-re.sub(pattern, 'XXX', 're Syndroom van Tietze re # bla re', flags=False)
-
-df_dummy = pd.DataFrame(['Syndroom van Tietze re #'],columns=['diagnose'])
-df_dummy['text'] = df_dummy['diagnose'].progress_apply(lambda t: replace_abbreviations(t,ab))
-
-replace_abbreviations('Syndroom van MDL Tietze re #',abbr_list)
-
-
-replace_abbreviations_in_column(df_dummy,'diagnose',abbreviations_df)
-
-df_dummy.head()
-
-d = 'SADASD'
-d= d.lower()
