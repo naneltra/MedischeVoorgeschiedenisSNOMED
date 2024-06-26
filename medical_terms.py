9dd6bd61de76e842348faa5e3f5c02ce9031761c
Lennaart Raams <l.raams@gerimedica.nl>
2024-06-26 20:20:59 +0200

    improved keyword replacement, but very slow

--- medical_terms.py
+++ medical_terms.py
@@ -4,89 +4,56 @@ This will improve annotation quality, the annotation model is setup to not annot
 """
 
 
-import csv
 import pandas as pd
+import re
+from tqdm import tqdm
+tqdm.pandas(desc="processing vumc dataextract")
+
+# df.to_csv('terms.csv',index=None)
+abbreviations_df = pd.read_csv('./data/terms.csv',index_col=None)
+abbreviations_df = abbreviations_df.loc[abbreviations_df['status']=='enable']
+abbreviations_df.info()
+
+abbr_list = abbreviations_df.to_dict(orient='records')
 
 
-def read_tsv(file_path):
-    data = []
-    try:
-        with open(file_path, mode='r', newline='', encoding='utf-8') as tsv_file:
-            reader = csv.DictReader(tsv_file, delimiter='\t')
-            for row in reader:
-                data.append(row)
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    return data
+def replace_abbreviations(text:str, abbreviations_df):
+    for abbr in abbr_list:
+        abbreviation = abbr['term']
+        replacement = abbr['meaning']
+        ignore_case = abbr.get('ignore_case', False)
 
-def read_two_column_file(file_path):
-    data = []
-    try:
-        with open(file_path, mode='r', encoding='utf-8') as file:
-            for line in file:
-                parts = line.split(' ', 1)  # Split on the first space
-                if len(parts) == 2:
-                    data.append({'term': parts[0], 'meaning':parts[1].strip(), 'remark':''})  # Strip any trailing whitespace from the second column
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    return data
-
-
-file_path = './gebruikte_termen.csv'
-tsv_data = read_tsv(file_path)
-for row in tsv_data:
-    print(row)
-
-file_path = './gebruikte_termen2.csv'
-file_data = read_two_column_file(file_path)
-for row in file_data:
-    print(row)
-
-file_data.extend(tsv_data)
-
-file_data = sorted(file_data, key=lambda x: x['term'], reverse=False)
-
-for d in file_data:
-    d |= {'status':'enable','case':True}
-
-
-df = pd.DataFrame.from_dict(file_data)
-
-df.to_csv('terms.csv',index=None)
-abbreviations_df = pd.read_csv('./terms.csv',index_col=None)
-abbreviations_df = df.loc[df['status']=='enable']
-abbreviations_df.shape
-
-
-def replace_abbreviations_in_column(df, abbreviations_df):
-
-    for index, row in abbreviations_df.iterrows():
-        abbreviation = row['term']
-        replacement = row['meaning']
-        ignore_case = row.get('ignore_case', False)
-
-        # Create regex pattern based on case sensitivity
         flags = re.IGNORECASE if ignore_case else 0
-        pattern = r'\b{}\b'.format(re.escape(abbreviation))
+
+        pattern = r'(?=<^|\s|){}(?=[.><+]|\s|$)'.format(re.escape(abbreviation))
 
         # Replace abbreviations in the column using lambda function
-        df['text'] = df['text'].apply(lambda text: re.sub(pattern, replacement, text, flags=flags))
+        # df['text'] = df[label].progress_apply(lambda text: re.sub(pattern, replacement, text, flags=flags))
+        
+        text = re.sub(pattern,replacement, text, flags=flags)
 
-    return df
+    return text
+
 
 
 df_text = pd.DataFrame(['this is a text', 'this is another text'], columns=['text'])
-df_text.head()
+df_text = pd.read_csv('./data/medische_voorgeschiedenis.csv', index_col='db_id', sep='|')
+df_text = df_text.loc[~df_text['diagnose'].isna()]
 
-replace_abbreviations_in_column(df_text,'text',df)
+df_text['text'] = df_text['diagnose'].progress_apply(lambda text: replace_abbreviations(text,abbreviations_df))
+
+pattern = r'(?=<^|\s|){}(?=[.><+]|\s|$)'.format(re.escape('re'))
+re.sub(pattern, 'XXX', 're Syndroom van Tietze re # bla re', flags=False)
+
+df_dummy = pd.DataFrame(['Syndroom van Tietze re #'],columns=['diagnose'])
+df_dummy['text'] = df_dummy['diagnose'].progress_apply(lambda t: replace_abbreviations(t,ab))
+
+replace_abbreviations('Syndroom van MDL Tietze re #',abbr_list)
 
 
-df = df.rename(columns={'case':'ignore_case'})
+replace_abbreviations_in_column(df_dummy,'diagnose',abbreviations_df)
 
-df['ignore_case'] = df['ignore_case'].apply(lambda x: not x)
+df_dummy.head()
 
-df.head()
+d = 'SADASD'
+d= d.lower()
